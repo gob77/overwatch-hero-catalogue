9961c9fb9a7de4017b0033fe5aca71d1ab2a64bb
Matias Fernandez Martinez <thematho@gmail.com>
2017-02-06 22:44:29 -0300

    Improvement suggestions. - JS optimization - Fixed issue with displays on image holders. - Moved classes to image-holder instead of each image and name

--- gamescript2.js
+++ gamescript2.js
@@ -1,104 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
 	var selectedClass = document.getElementById("selectedClass");
-	selectedClass.addEventListener("change", function(){
-		if(selectedClass.value === "tank"){
-			var hideClass = document.getElementsByTagName("img");
-			var charName = document.getElementsByClassName("characterName");
-			for(var i = 0; i < hideClass.length; i++){
-				if(hideClass[i].classList.contains("offense")){
-hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("deffense")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("support")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("tank")){
-					hideClass[i].classList.remove("displayNone");
-					charName[i].classList.remove("displayNone");
-				}
-			}
-		}
-		else if(selectedClass.value === "offense"){
-			var hideClass = document.getElementsByTagName("img");
-			var charName = document.getElementsByClassName("characterName");
-			for(var i = 0; i < hideClass.length; i++){
-				if(hideClass[i].classList.contains("tank")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("deffense")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("support")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("offense")){
-					hideClass[i].classList.remove("displayNone");
-					charName[i].classList.remove("displayNone");
-				}
-			}
-		}
-		else if(selectedClass.value === "deffense"){
-			var hideClass = document.getElementsByTagName("img");
-			var charName = document.getElementsByClassName("characterName");
-			for(var i = 0; i < hideClass.length; i++){
-				if(hideClass[i].classList.contains("offense")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("tank")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("support")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("deffense")){
-					hideClass[i].classList.remove("displayNone");
-					charName[i].classList.remove("displayNone");
-				}
-			}
-		}
-		else if(selectedClass.value === "support"){
-			var hideClass = document.getElementsByTagName("img");
-			var charName = document.getElementsByClassName("characterName");
-			for(var i = 0; i < hideClass.length; i++){
-				if(hideClass[i].classList.contains("offense")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("deffense")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("tank")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("support")){
-					hideClass[i].classList.remove("displayNone");
-					charName[i].classList.remove("displayNone");
-				}
-			}
-		}
-		if(selectedClass.value === "Show All"){
-			var hideClass = document.getElementsByTagName("img");
-			var charName = document.getElementsByClassName("characterName");
-			for(var i = 0; i < hideClass.length; i++){
-				if(hideClass[i].classList.contains("displayNone")){
-					hideClass[i].classList.remove("displayNone");
-					charName[i].classList.remove("displayNone");
-				}
-			}
-		}
 
-	})
-})
+	selectedClass.addEventListener("change", function() {
+		var i, image,
+		type = this.value,
+		images = document.getElementsByClassName("image-holder");
+
+		for(i = 0; i < images.length; i++) {
+			image = images[i].classList;
+
+			this.value === 'all' || image.contains(this.value) ?
+				image.remove("displayNone") : image.add("displayNone");
+		}
+	});
+
+});
