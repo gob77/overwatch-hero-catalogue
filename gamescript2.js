98148a186f26a40f759e870f148a77d303320df5
gob77 <mraortiz@hotmail.com>
2017-02-10 11:25:04 -0300

    rewrite js file for img filter

--- gamescript2.js
+++ gamescript2.js
@@ -1,104 +1,53 @@
 document.addEventListener("DOMContentLoaded", function(){
 	var selectedClass = document.getElementById("selectedClass");
 	selectedClass.addEventListener("change", function(){
-		if(selectedClass.value === "tank"){
-			var hideClass = document.getElementsByTagName("img");
-			var charName = document.getElementsByClassName("characterName");
-			for(var i = 0; i < hideClass.length; i++){
+		var hideClass = document.getElementsByClassName("overwatch");
+		var charName = document.getElementsByClassName("characterName");
+		for(var  i = 0; i < hideClass.length; i++){
+			if(selectedClass.value === "offense"){
 				if(hideClass[i].classList.contains("offense")){
-hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("deffense")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("support")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("tank")){
 					hideClass[i].classList.remove("displayNone");
 					charName[i].classList.remove("displayNone");
 				}
+				else{
+					hideClass[i].classList.add("displayNone");
+					charName[i].classList.add("displayNone");
+				}
 			}
-		}
-		else if(selectedClass.value === "offense"){
-			var hideClass = document.getElementsByTagName("img");
-			var charName = document.getElementsByClassName("characterName");
-			for(var i = 0; i < hideClass.length; i++){
+			else if (selectedClass.value === "deffense"){
+				if (hideClass[i].classList.contains("deffense")){
+					hideClass[i].classList.remove("displayNone");
+					charName[i].classList.remove("displayNone");
+				}
+				else{
+					hideClass[i].classList.add("displayNone");
+					charName[i].classList.add("displayNone");
+				}
+			}
+			else if(selectedClass.value === "tank"){
 				if(hideClass[i].classList.contains("tank")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("deffense")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("support")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("offense")){
 					hideClass[i].classList.remove("displayNone");
 					charName[i].classList.remove("displayNone");
 				}
+				else{
+					hideClass[i].classList.add("displayNone");
+					charName[i].classList.add("displayNone");
+				}
 			}
-		}
-		else if(selectedClass.value === "deffense"){
-			var hideClass = document.getElementsByTagName("img");
-			var charName = document.getElementsByClassName("characterName");
-			for(var i = 0; i < hideClass.length; i++){
-				if(hideClass[i].classList.contains("offense")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("tank")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("support")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("deffense")){
+			else if(selectedClass.value === "support"){
+				if(hideClass[i].classList.contains("support")){
 					hideClass[i].classList.remove("displayNone");
 					charName[i].classList.remove("displayNone");
 				}
-			}
-		}
-		else if(selectedClass.value === "support"){
-			var hideClass = document.getElementsByTagName("img");
-			var charName = document.getElementsByClassName("characterName");
-			for(var i = 0; i < hideClass.length; i++){
-				if(hideClass[i].classList.contains("offense")){
+				else{
 					hideClass[i].classList.add("displayNone");
 					charName[i].classList.add("displayNone");
 				}
-				else if(hideClass[i].classList.contains("deffense")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("tank")){
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-				else if(hideClass[i].classList.contains("support")){
-					hideClass[i].classList.remove("displayNone");
-					charName[i].classList.remove("displayNone");
-				}
+			}
+			else{
+				hideClass[i].classList.remove("displayNone");
+				charName[i].classList.remove("displayNone");
 			}
 		}
-		if(selectedClass.value === "Show All"){
-			var hideClass = document.getElementsByTagName("img");
-			var charName = document.getElementsByClassName("characterName");
-			for(var i = 0; i < hideClass.length; i++){
-				if(hideClass[i].classList.contains("displayNone")){
-					hideClass[i].classList.remove("displayNone");
-					charName[i].classList.remove("displayNone");
-				}
-			}
-		}
-
 	})
 })
