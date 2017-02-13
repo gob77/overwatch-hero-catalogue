70af07662a3ffbc6c3ad56a8a1e873762ec58756
gob77 <mraortiz@hotmail.com>
2017-02-13 13:28:09 -0300

    re write js, eliminated unnecessary validation, eliminated <br> fron html layout

--- gamescript2.js
+++ gamescript2.js
@@ -1,53 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(){
 	var selectedClass = document.getElementById("selectedClass");
 	selectedClass.addEventListener("change", function(){
-		var hideClass = document.getElementsByClassName("overwatch");
-		var charName = document.getElementsByClassName("characterName");
-		for(var  i = 0; i < hideClass.length; i++){
-			if(selectedClass.value === "offense"){
-				if(hideClass[i].classList.contains("offense")){
-					hideClass[i].classList.remove("displayNone");
-					charName[i].classList.remove("displayNone");
-				}
-				else{
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-			}
-			else if (selectedClass.value === "deffense"){
-				if (hideClass[i].classList.contains("deffense")){
-					hideClass[i].classList.remove("displayNone");
-					charName[i].classList.remove("displayNone");
-				}
-				else{
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-			}
-			else if(selectedClass.value === "tank"){
-				if(hideClass[i].classList.contains("tank")){
-					hideClass[i].classList.remove("displayNone");
-					charName[i].classList.remove("displayNone");
-				}
-				else{
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-			}
-			else if(selectedClass.value === "support"){
-				if(hideClass[i].classList.contains("support")){
-					hideClass[i].classList.remove("displayNone");
-					charName[i].classList.remove("displayNone");
-				}
-				else{
-					hideClass[i].classList.add("displayNone");
-					charName[i].classList.add("displayNone");
-				}
-			}
-			else{
+
+		var hideClass = document.getElementsByClassName("image-holder");
+		for(var i = 0; i < hideClass.length; i++){
+
+			if(selectedClass.value === "all"){
 				hideClass[i].classList.remove("displayNone");
-				charName[i].classList.remove("displayNone");
+			}
+			else if(selectedClass.value === selectedClass.value){
+				if(hideClass[i].classList.contains(selectedClass.value)){
+					hideClass[i].classList.remove("displayNone");
+				}
+				else{
+					hideClass[i].classList.add("displayNone");
+				}
 			}
 		}
 	})
-});
+})
